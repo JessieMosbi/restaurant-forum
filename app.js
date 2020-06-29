1883c95c811bf096fefd8780efed62665e92b2ae
JessieCheng <hamy820326@gmail.com>
2020-06-29 23:19:19 +0800

    Install bcryptjs and body-parser

--- app.js
+++ app.js
@@ -5,11 +5,15 @@ const PORT = process.env.PORT || 3000
 // modules and files
 const express = require('express')
 const handlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
 
 // web server settings
 const app = express()
 app.engine('.hbs', handlebars({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', '.hbs')
+
+// other middleware
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // database settings
 const db = require('./models/index.js')
