421d48e53dcaf6ce9033c1c914e35daf56f5f54c
JessieCheng <hamy820326@gmail.com>
2020-06-29 22:15:10 +0800

    Add restController and restaurant index page

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
+// params
 if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 const PORT = process.env.PORT || 3000
 
+// modules and files
 const express = require('express')
 const handlebars = require('express-handlebars')
 
+// web server settings
 const app = express()
-app.engine('handlebars', handlebars({ defaultLayout: 'main', extname: '.hbs' }))
-app.set('view engine', 'handlebars')
+app.engine('.hbs', handlebars({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', '.hbs')
 
+// routes
+require('./routes/index.js')(app)
+
+// start web server
 app.listen(PORT, () => {
   console.log(`web server start at port ${PORT}`)
 })
