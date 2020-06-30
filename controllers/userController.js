2accad7901699ea8117a06f6450fccc95a4ad0d4
JessieCheng <hamy820326@gmail.com>
2020-06-30 22:01:13 +0800

    Confirm user password and unique email

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,12 +7,26 @@ const userController = {
     return res.render('signup')
   },
   signUp: (req, res) => {
-    User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
-    })
+    // confirm password
+    if (req.body.passwordCheck !== req.body.password) {
+      req.flash('error_messages', '兩次密碼輸入不同！')
+      return res.redirect('/signup')
+    }
+    // confirm unique user email
+    User.findOne({ where: { email: req.body.email } })
       .then(user => {
+        if (user) {
+          req.flash('error_messages', '信箱重複！')
+          return res.redirect('/signup')
+        }
+        return User.create({
+          name: req.body.name,
+          email: req.body.email,
+          password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
+        })
+      })
+      .then(user => {
+        req.flash('success_messages', '成功註冊帳號！')
         return res.redirect('/signin')
       })
   }
