fb6c8a843dc5315565d1ee836cf0cf29b7d3286a
JessieCheng <hamy820326@gmail.com>
2020-07-02 21:33:29 +0800

    Add admin restaurant post feature

--- routes/index.js
+++ routes/index.js
@@ -21,9 +21,12 @@ module.exports = (app, passport) => {
 
   app.get('/', authenticated, (req, res) => res.redirect('/restaurants'))
   app.get('/restaurants', authenticated, restController.getRestaurants)
+
   app.get('/admin/', authenticatedAdmin, (req, res) => res.redirect('/admin/restaurants'))
   app.get('/admin/restaurants', authenticatedAdmin, adminController.getRestaurants)
   app.get('/admin/restaurants/create', authenticatedAdmin, adminController.createRestaurant)
+  app.post('/admin/restaurants', authenticatedAdmin, adminController.postRestaurant)
+
   app.get('/signup', userController.signUpPage)
   app.post('/signup', userController.signUp)
   app.get('/signin', userController.signInPage)
