2295f607000399d66d60c56d57a4e056302c72ac
JessieCheng <hamy820326@gmail.com>
2020-07-07 23:39:15 +0800

    feat: Add handlebar helper ifCond

--- config/handlebarsHelper.js
+++ config/handlebarsHelper.js
@@ -1,5 +1,12 @@
 module.exports = {
   ifEqual: (value1, value2) => {
     return String(value1) === String(value2)
+  },
+
+  ifCond: function (a, b, options) {
+    if (a === b) {
+      return options.fn(this)
+    }
+    return options.inverse(this)
   }
 }
